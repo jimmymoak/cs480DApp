750c420e2244281100e71552f0f7acd3de0f9f30
yurbudsam <92602382+yurbudsam@users.noreply.github.com>
2024-04-17 13:08:40 -0700

    Update Adoption.sol
    made it rent

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
-contract Adoption {
-    // holds the adopters, max 16
+contract Rental{
+
     address[16] public adopters;
-	uint256 public adoptionPrice = 0.5;
 
-	// need exact amount to adopt because there is no money back method
-	function adopt(uint petId) public payable returns (uint) {
-		require(petId >= 0 && petId < 16, "Pet ID must be within range 0 to 15");
-		require(msg.value == adoptionPrice, "Must send exact adoption price");
-		
-		adopters[petId] = msg.sender;
+// rent function
+    function rent(uint bookId) public returns (uint){
+        require(bookId >= 0 && petId <= 15);
 
-		return petId;
-	}
-	
-    function getAdopters() public view returns (address[16] memory) {
-        return adopters;
+        renters[bookId] = msg.sender;
+
+        return bookId;
     }
+// retrieve the renters
+    function getRenters() public view returns (address[16] memory){
+        return renters;
+    }
+
 }
